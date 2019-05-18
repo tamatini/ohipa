2d5dc47414994853e97a5a21f599a6e7f59eaa67
TEAHUI <tamatini@hotmail.fr>
2019-05-17 23:06:26 -1000

    maj heroku

---
+++ config.py
@@ -0,0 +1,4 @@
+SQLALCHEMY_DATABASE_URI = 'postgres://seshapuxkazdgs:00be0be81db5ec2cbc1e0d61b9667d7f75e7a30de972e1eb9989d975774f7e67@ec2-54-243-197-120.compute-1.amazonaws.com:5432/dfo1a9v1hqlgus'
+ENV = 'Production'
+DEBUG = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
